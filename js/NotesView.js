0ba1ad02ed40a22016e125f30d96d5f3524db9dc
mahsa-fotros <fotros.mahsa@gmail.com>
2023-02-18 15:05:33 +0330

    made date persian

--- js/NotesView.js
+++ js/NotesView.js
@@ -1,7 +1,7 @@
 export default class NotesView {
   constructor(root, handler) {
     this.root = root;
-    const { onNoteAdd, onNoteEdit, onNoteSelect, onNoteDelete} = handler;
+    const { onNoteAdd, onNoteEdit, onNoteSelect, onNoteDelete } = handler;
     this.onNoteAdd = onNoteAdd;
     this.onNoteEdit = onNoteEdit;
     this.onNoteSelect = onNoteSelect;
@@ -40,18 +40,15 @@ export default class NotesView {
     //hide note preview in first loading
     this.updateNotePreviewVisibility(false);
   }
-
+// ${new persianDate(updated).toLocaleString("en", {
+//      dateStyle: "full",
+//      timeStyle: "short",
+//    })}
   _createListItemHTML(id, title, body, updated) {
-      return `<div class="notes__list-item" data-note-id="${id}">
+    return `<div class="notes__list-item" data-note-id="${id}">
       <div class="notes__item-title">${title}</div>
       <div class="notes__item-text">${body}</div>
-      <div class="note__item-date">${new Date(updated).toLocaleString(
-          "en",
-                  {
-                    dateStyle: "full",
-                    timeStyle: "short",
-                  }
-                )}</div>
+      <div class="note__item-date">${new Date(updated).toLocaleString("fa-IR")}</div>
                 <div class="notes__item-trash" data-note-id="${id}">
                 <i class="fas fa-trash-alt fa-lg"></i>
                 </div>
@@ -60,42 +57,47 @@ export default class NotesView {
 
   updateNotesList(notes) {
     const notesContainer = this.root.querySelector(".notes__list");
-    
-    notesContainer.innerHTML="";
-    let notesList="";
-    for(const note of notes){
-        const {id,title,body,updated}= note;
-        const noteHtml= this._createListItemHTML(id,title,body,updated);
-        notesList+=noteHtml;
+
+    notesContainer.innerHTML = "";
+    let notesList = "";
+    for (const note of notes) {
+      const { id, title, body, updated } = note;
+      const noteHtml = this._createListItemHTML(id, title, body, updated);
+      notesList += noteHtml;
     }
-    notesContainer.innerHTML=notesList;
-    notesContainer.querySelectorAll(".notes__list-item").forEach(noteItem=>{
-        noteItem.addEventListener("click",()=>{
-            this.onNoteSelect(noteItem.dataset.noteId);
-        })
+    notesContainer.innerHTML = notesList;
+    notesContainer.querySelectorAll(".notes__list-item").forEach((noteItem) => {
+      noteItem.addEventListener("click", () => {
+        this.onNoteSelect(noteItem.dataset.noteId);
+      });
     });
 
-   notesContainer.querySelectorAll(".notes__item-trash").forEach(noteItem=>{
-    noteItem.addEventListener("click",(e)=>{
-        e.stopPropagation();
-        this.onNoteDelete(noteItem.dataset.noteId);
-    })
-   });
-
+    notesContainer
+      .querySelectorAll(".notes__item-trash")
+      .forEach((noteItem) => {
+        noteItem.addEventListener("click", (e) => {
+          e.stopPropagation();
+          this.onNoteDelete(noteItem.dataset.noteId);
+        });
+      });
   }
 
-  updateActiveNote(note){
+  updateActiveNote(note) {
     this.root.querySelector(".notes__title").value = note.title;
     this.root.querySelector(".notes__body").value = note.body;
 
     //add selected class
-    this.root.querySelectorAll(".notes__list-item").forEach(item=>{
+    this.root.querySelectorAll(".notes__list-item").forEach((item) => {
       item.classList.remove("notes__list-item--selected");
     });
-    this.root.querySelector(`.notes__list-item[data-note-id="${note.id}"]`).classList.add("notes__list-item--selected");
+    this.root
+      .querySelector(`.notes__list-item[data-note-id="${note.id}"]`)
+      .classList.add("notes__list-item--selected");
   }
 
-  updateNotePreviewVisibility(visible){
-    this.root.querySelector(".notes__preview").style.visibility= visible ? "visible" : "hidden";
+  updateNotePreviewVisibility(visible) {
+    this.root.querySelector(".notes__preview").style.visibility = visible
+      ? "visible"
+      : "hidden";
   }
 }
